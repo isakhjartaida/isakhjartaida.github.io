44f7f70dee6ee3df7a2b1ee13d4cc396216f66a7
Oskar Emilsson <oskaremilsson@gmail.com>
2020-10-07 19:41:12 +0200

    Updates

---
+++ precache-manifest.3b1088f604224fb2400d4e69922fc7fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5cf85018110920155109e9792008a9",
+    "url": "/isakhjartaida.github.io/index.html"
+  },
+  {
+    "revision": "c0977ea868eb5a7177ea",
+    "url": "/isakhjartaida.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f38e9d381343c135012",
+    "url": "/isakhjartaida.github.io/static/js/2.cefe79ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/isakhjartaida.github.io/static/js/2.cefe79ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0977ea868eb5a7177ea",
+    "url": "/isakhjartaida.github.io/static/js/main.cf99a0f7.chunk.js"
+  },
+  {
+    "revision": "6fde8a2e5517cdba3030",
+    "url": "/isakhjartaida.github.io/static/js/runtime-main.2f063848.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/isakhjartaida.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
