921d4c22ff13a9d12b2b62d2708916a337c39b92
Oskar Emilsson <oskaremilsson@gmail.com>
2020-10-08 21:27:15 +0200

    Add countdown

--- isakhjartaida/src/App.js
+++ isakhjartaida/src/App.js
@@ -1,36 +1,30 @@
 import React from 'react';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import { CssBaseline, Box } from '@material-ui/core';
+
+import { Box } from '@material-ui/core';
 import { Favorite as FavoriteIcon } from '@material-ui/icons';
 
-const theme = createMuiTheme({
-  palette: {
-    background: {
-      default: '#a3b899',
-    },
-    primary: {
-      main: '#f8d3c5',
-    },
-  },
-});
+import Theme from './Theme';
+import Time from './Time';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <Theme>
       <Box
         display="flex"
         justifyContent="center"
         alignItems="center"
         with="100vw"
         height="100vh"
+        flexDirection="column"
       >
         <FavoriteIcon
           color="primary"
           style={{ fontSize: 56 }}
         />
+
+        <Time />
       </Box>
-    </ThemeProvider>
+    </Theme>
   );
 }
 
---
+++ isakhjartaida/src/Time/Time.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Countdown from 'react-countdown';
+
+const cdUseStyles = makeStyles((theme) => ({
+  root: {
+    ...theme.typography,
+    fontFamily: ['charter, Roboto'],
+    fontSize: 24,
+  },
+}));
+
+const bdeUseStyles = makeStyles((theme) => ({
+  root: {
+    ...theme.typography,
+    fontFamily: ['charter, Roboto'],
+    fontSize: 32,
+  },
+}));
+
+export default function Time() {
+  const cdClasses = cdUseStyles();
+  const bdeClasses = bdeUseStyles();
+  
+  const renderer = ({ days }) => {
+    return (
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        { days > 0 &&
+          <Typography className={cdClasses.root}>
+            {days} days until
+          </Typography>
+        }
+        <Typography className={bdeClasses.root}>
+          BEST DAY EVER
+        </Typography>
+      </Box>
+    );
+  };
+
+  return (
+    <Box>
+      <Countdown
+        date={'2021-07-17'}
+        renderer={renderer}
+      />
+    </Box>
+  );
+}
