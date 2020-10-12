de835d6665372af8a914f863d20750665a279aec
Oskar Emilsson <oskaremilsson@gmail.com>
2020-10-12 16:51:37 +0200

    Use paper

--- isakhjartaida/src/NotifyMe/NotifyMe.js
+++ isakhjartaida/src/NotifyMe/NotifyMe.js
@@ -1,22 +1,20 @@
 import React, { useEffect, useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, TextField, Button, Typography } from '@material-ui/core';
+import { Box, TextField, Button, Typography, Paper } from '@material-ui/core';
 import axios from 'axios';
 
-const FORM_UL = 'https://cors-anywhere.herokuapp.com/https://docs.google.com/forms/d/e/1FAIpQLSfQC24BD9tqflJVnVsth96TFfZkhrFzAxdkMfTVjJ9loFCYow/formResponse';
+const FORM_URL = 'https://cors-anywhere.herokuapp.com/https://docs.google.com/forms/d/e/1FAIpQLSfQC24BD9tqflJVnVsth96TFfZkhrFzAxdkMfTVjJ9loFCYow/formResponse';
 
 const postGoogleForm = (email, setSend) => {
   let data = new FormData();
   data.append('emailAddress', email);
-  axios.post(FORM_UL, new URLSearchParams(data)).then((res) => {
-    console.log(res);
+  axios.post(FORM_URL, new URLSearchParams(data)).then((res) => {
     setSend('sent');
   });
 }
 
 const useStyles = makeStyles((theme) => ({
   NotifyMe: {
-    background: theme.palette.background.paper,
     padding: theme.spacing(2),
   },
 }));
@@ -28,49 +26,50 @@ export default function NotifyMe() {
   const [send, setSend] = useState(undefined);
 
   useEffect(() => {
-    console.log(send);
     if (email && email.length > 0 && send === 'send') {
-      console.log(email);
       postGoogleForm(email, setSend)
     }
   }, [email, send]);
 
   return (
-    <Box
-      width="100%"
-      className={classes.NotifyMe}
-    >
-      <Box marginBottom={3}>
-        <Typography>
-          Fyll i din email-address för att få ett email när det finns ny information, tex. när OSA öppnar <span role="img" aria-label="Tada">🎉</span>
-        </Typography>
-      </Box>
-      { send === 'sent' ?
-        <Typography>Tack!</Typography>
-      :
-        <Box display="flex" flexDirection="column" alignItems="center">
-          <TextField
-            id="email"
-            label="e-mail"
-            placeholder="Skriv din email här"
-            variant="outlined"
-            fullWidth
-            InputLabelProps={{
-              shrink: true,
-            }}
-            onChange={(e) => { setEmail(e.target.value) }}
-          />
-          <Box margin={2}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => { setSend('send') }}
-            >
-              Meddela mig
-            </Button>
-          </Box>
+    <Box width="100%">
+      <Paper className={classes.NotifyMe}>
+        <Box marginBottom={3}>
+          <Typography>
+            Fyll i din email-address för att få ett email när det finns ny information, tex. när OSA öppnar <span role="img" aria-label="Tada">🎉</span>
+          </Typography>
         </Box>
-      }
+        { send === 'sent' ?
+          <Typography>Tack!</Typography>
+        :
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="flex-end"
+          >
+            <TextField
+              id="email"
+              label="e-mail"
+              placeholder="Skriv din email här"
+              variant="outlined"
+              fullWidth
+              InputLabelProps={{
+                shrink: true,
+              }}
+              onChange={(e) => { setEmail(e.target.value) }}
+            />
+            <Box margin={2}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => { setSend('send') }}
+              >
+                Meddela mig
+              </Button>
+            </Box>
+          </Box>
+        }
+      </Paper>
     </Box>
   );
 }
