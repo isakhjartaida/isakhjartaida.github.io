1aa0befdb933924458cb1f836af9c2dc9e355bbd
Oskar Emilsson <oskaremilsson@gmail.com>
2020-10-20 21:30:32 +0200

    Adding flowers to save the date

--- isakhjartaida/src/AddToCalendar/AddToCalendar.js
+++ isakhjartaida/src/AddToCalendar/AddToCalendar.js
@@ -5,6 +5,8 @@ import { List, ListSubheader, ListItem, ListItemIcon, ListItemText } from '@mate
 
 import { AndroidOutlined, Apple, EventOutlined, EventAvailableOutlined } from '@material-ui/icons';
 
+import { ReactComponent as Flowers } from 'assets/krans.svg';
+
 const downloadICal = (data) => {
   let link = document.createElement('a');
   link.href = `data:text/calendar;charset=utf-8,${encodeURIComponent(data)}`;
@@ -59,15 +61,33 @@ export default function AddToCalendar() {
       marginTop={4}
       marginBottom={4}
     >
-      <Button
-        variant="contained"
-        onClick={()=>{setState(true)}}
-        startIcon={<EventAvailableOutlined />}
-        size="large"
-        className={classes.button}
+      <Box
+        position="relative"
       >
-        Save the date
-      </Button>
+        <Flowers
+          height={300}
+          width={400}
+        />
+
+        <Box
+          position="absolute"
+          bottom="80px"
+          left={0}
+          width={400}
+          display="flex"
+          justifyContent="center"
+        >
+          <Button
+            variant="contained"
+            onClick={()=>{setState(true)}}
+            startIcon={<EventAvailableOutlined />}
+            size="large"
+            className={classes.button}
+          >
+            Save the date
+          </Button>
+        </Box>
+      </Box>
 
       <Drawer
         anchor={'bottom'}
